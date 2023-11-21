744d4db6d760f61166e8ea2281275848dddac651
musella95 <musellamatteoo@hotmail.com>
2023-11-21 17:31:17 +0100

    mile stone 1 done

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
 //mile stone 0:Creare l’array di oggetti con le informazioni fornite nella tabella qui sotto
 let infoDipendenti = [
 {
-    nome: "wayne Barnet"
-    role: "Founder & Ceo"
+    nome: "wayne Barnet",
+    role: "Founder & Ceo",
     image: "wayne-barnett-founder-ceo.jpg"
 },
 {
-    nome: "Angela Caroll"
-    role: "Chief Editor"
+    nome: "Angela Caroll",
+    role: "Chief Editor",
     image: "angela-caroll-chief-editor.jpg"
 },
 {
-    nome: "Walter Gordon"
-    role: "Officer Manager"
+    nome: "Walter Gordon",
+    role: "Officer Manager",
     image: "walter-gordon-office-manager.jpg"
 },
 {
-    nome: "Angela Lopez"
-    role: "Social Media Manager"
+    nome: "Angela Lopez",
+    role: "Social Media Manager",
     image: "angela-lopez-social-media-manager.jpg"
 },
 {
-    nome: "Scott Estrada"
-    role: "Developer"
+    nome: "Scott Estrada",
+    role: "Developer",
     image: "scott-estrada-developer.jpg"
 },
 {
-    nome: "Barbara Ramos"
-    role: "Grapich Designer"
+    nome: "Barbara Ramos",
+    role: "Grapich Designer",
     image: "barbara-ramos-graphic-designer.jpg"
 },
 ]
+// mile stone 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+console.log(infoDipendenti);
+
+//MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
