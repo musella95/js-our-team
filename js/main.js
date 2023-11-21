22e86550941f524a4841c8d795fc54139e923e22
musella95 <musellamatteoo@hotmail.com>
2023-11-21 17:01:02 +0100

    mile stone 0

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//mile stone 0:Creare l’array di oggetti con le informazioni fornite nella tabella qui sotto
+let infoDipendenti = [
+{
+    nome: "wayne Barnet"
+    role: "Founder & Ceo"
+    image: "wayne-barnett-founder-ceo.jpg"
+},
+{
+    nome: "Angela Caroll"
+    role: "Chief Editor"
+    image: "angela-caroll-chief-editor.jpg"
+},
+{
+    nome: "Walter Gordon"
+    role: "Officer Manager"
+    image: "walter-gordon-office-manager.jpg"
+},
+{
+    nome: "Angela Lopez"
+    role: "Social Media Manager"
+    image: "angela-lopez-social-media-manager.jpg"
+},
+{
+    nome: "Scott Estrada"
+    role: "Developer"
+    image: "scott-estrada-developer.jpg"
+},
+{
+    nome: "Barbara Ramos"
+    role: "Grapich Designer"
+    image: "barbara-ramos-graphic-designer.jpg"
+},
+]
